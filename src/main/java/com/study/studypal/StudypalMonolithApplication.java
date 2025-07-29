6157aac40e438f47180eabef341f211f9aa57c7e
Test_repo <22520992@gm.uit.edu.vn>
2025-07-29 17:22:15 +0700

    feat: Initialize project.

---
+++ src/main/java/com/study/studypal/StudypalMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.study.studypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudypalMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudypalMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/studypal/StudypalMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudypalMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
