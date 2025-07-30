abf223bf65dcb179691748b3fe43bb98fc0036db
Test_repo <22520992@gm.uit.edu.vn>
2025-07-30 14:30:02 +0700

    fix: fix openAPI and security bugs.

--- src/main/java/com/study/studypal/security/SecurityConfig.java
+++ src/main/java/com/study/studypal/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,14 +29,18 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.requireCsrfProtectionMatcher(request -> false))
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth ->{
                     auth.requestMatchers(
                             "/swagger/**",
+                            "/swagger-ui/**",
                             "/v3/api-docs/**",
                             "/swagger-resources/**",
+                            "/webjars/**",
+                            "/h2-console/**",
                             "/api/auth/**"
                     ).permitAll();
                 });
