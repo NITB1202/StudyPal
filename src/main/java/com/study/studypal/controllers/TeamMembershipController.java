9670f953ab69e3454080c6dd820ef9a887614738
Test_repo <22520992@gm.uit.edu.vn>
2025-08-01 15:43:29 +0700

    feat: add team member controller.

---
+++ src/main/java/com/study/studypal/controllers/TeamMembershipController.java
@@ -0,0 +1,102 @@
+package com.study.studypal.controllers;
+
+import com.study.studypal.dtos.Shared.ActionResponseDto;
+import com.study.studypal.dtos.TeamUser.request.RemoveTeamMemberRequestDto;
+import com.study.studypal.dtos.TeamUser.request.UpdateMemberRoleRequestDto;
+import com.study.studypal.dtos.TeamUser.response.ListTeamMemberResponseDto;
+import com.study.studypal.dtos.TeamUser.response.UserRoleInTeamResponseDto;
+import com.study.studypal.exceptions.ErrorResponse;
+import com.study.studypal.services.TeamMembershipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/api/members")
+public class TeamMembershipController {
+    private final TeamMembershipService teamMembershipService;
+
+    @PostMapping
+    @Operation(summary = "Join a team by team code.")
+    @ApiResponse(responseCode = "200", description = "Join successfully.")
+    @ApiResponse(responseCode = "404", description = "Not found.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public ResponseEntity<ActionResponseDto> joinTeam(@AuthenticationPrincipal UUID userId,
+                                                      @RequestParam String teamCode){
+        return ResponseEntity.ok(teamMembershipService.joinTeam(userId, teamCode));
+    }
+
+    @GetMapping
+    @Operation(summary = "Get the user's role in a team.")
+    @ApiResponse(responseCode = "200", description = "Get successfully.")
+    @ApiResponse(responseCode = "404", description = "Not found.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public ResponseEntity<UserRoleInTeamResponseDto> getUserRoleInTeam(@AuthenticationPrincipal UUID userId,
+                                                                       @RequestParam UUID teamId){
+        return ResponseEntity.ok(teamMembershipService.getUserRoleInTeam(userId, teamId));
+    }
+
+    @GetMapping("/all")
+    @Operation(summary = "Get a list of team members.")
+    @ApiResponse(responseCode = "200", description = "Get successfully.")
+    public ResponseEntity<ListTeamMemberResponseDto> getTeamMembers(@RequestParam UUID teamId,
+                                                                    @RequestParam(required = false) String cursor,
+                                                                    @RequestParam(defaultValue = "10") @Positive int size){
+        return ResponseEntity.ok(teamMembershipService.getTeamMembers(teamId, cursor, size));
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "Search for members by name.")
+    @ApiResponse(responseCode = "200", description = "Search successfully.")
+    public ResponseEntity<ListTeamMemberResponseDto> searchTeamMembersByUsername(@AuthenticationPrincipal UUID userId,
+                                                                                 @RequestParam UUID teamId,
+                                                                                 @RequestParam String keyword,
+                                                                                 @RequestParam(required = false) UUID cursor,
+                                                                                 @RequestParam(defaultValue = "10") @Positive int size){
+        return ResponseEntity.ok(teamMembershipService.searchTeamMembersByName(userId, teamId, keyword, cursor, size));
+    }
+
+    @PatchMapping
+    @Operation(summary = "Update the role of a specific team member.")
+    @ApiResponse(responseCode = "200", description = "Update successfully.")
+    @ApiResponse(responseCode = "400", description = "Invalid request body.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Not found.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public ResponseEntity<ActionResponseDto> updateTeamMemberRole(@AuthenticationPrincipal UUID userId,
+                                                                  @Valid @RequestBody UpdateMemberRoleRequestDto request){
+        return ResponseEntity.ok(teamMembershipService.updateTeamMemberRole(userId, request));
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Remove a member from the team.")
+    @ApiResponse(responseCode = "200", description = "Delete successfully.")
+    @ApiResponse(responseCode = "404", description = "Not found.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public ResponseEntity<ActionResponseDto> removeTeamMember(@AuthenticationPrincipal UUID userId,
+                                                              @Valid @RequestBody RemoveTeamMemberRequestDto request){
+        return ResponseEntity.ok(teamMembershipService.removeTeamMember(userId, request));
+    }
+
+    @DeleteMapping("/leave")
+    @Operation(summary = "Leave a team.")
+    @ApiResponse(responseCode = "200", description = "Leave successfully.")
+    @ApiResponse(responseCode = "404", description = "Not found.",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    public ResponseEntity<ActionResponseDto> leaveTeam(@AuthenticationPrincipal UUID userId,
+                                                       @RequestParam UUID teamId){
+        return ResponseEntity.ok(teamMembershipService.leaveTeam(userId, teamId));
+    }
+}
