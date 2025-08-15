adbb1a9b09ca5a215b88b3c5f582b2c61fdb4501
Test_repo <22520992@gm.uit.edu.vn>
2025-08-15 18:26:37 +0700

    refactor: reorganize test packages by module.

--- src/main/java/com/study/studypal/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/study/studypal/auth/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,10 @@
 package com.study.studypal.auth.filter;
 
+import com.study.studypal.auth.config.JwtAuthenticationEntryPoint;
 import com.study.studypal.auth.enums.AccountRole;
 import com.study.studypal.auth.exception.AuthErrorCode;
+import com.study.studypal.auth.exception.JwtAuthenticationException;
 import com.study.studypal.common.cache.CacheNames;
-import com.study.studypal.common.exception.BaseException;
 import com.study.studypal.common.util.CacheKeyUtils;
 import com.study.studypal.auth.util.JwtUtils;
 import jakarta.servlet.FilterChain;
@@ -25,38 +26,51 @@ import java.util.UUID;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     private final CacheManager cacheManager;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+
     private static final String AUTH_PREFIX = "/api/auth";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String accessToken = authHeader.substring(7);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String accessToken = authHeader.substring(7);
 
-            UUID userId = JwtUtils.extractId(accessToken);
-            AccountRole role = JwtUtils.extractAccountRole(accessToken);
+                UUID userId = JwtUtils.extractId(accessToken);
+                AccountRole role = JwtUtils.extractAccountRole(accessToken);
 
-            String path = request.getRequestURI();
-            if (!path.startsWith(AUTH_PREFIX)) {
-                String storedAccessToken = cacheManager.getCache(CacheNames.ACCESS_TOKENS).get(CacheKeyUtils.of(userId), String.class);
-                if (storedAccessToken == null) {
-                    throw new BaseException(AuthErrorCode.INVALID_ACCESS_TOKEN);
+                String path = request.getRequestURI();
+                if (!path.startsWith(AUTH_PREFIX)) {
+                    String storedAccessToken = cacheManager.getCache(CacheNames.ACCESS_TOKENS)
+                            .get(CacheKeyUtils.of(userId), String.class);
+
+                    if (storedAccessToken == null) {
+                        throw new JwtAuthenticationException(AuthErrorCode.INVALID_ACCESS_TOKEN);
+                    }
+
+                    if (!storedAccessToken.equals(accessToken)) {
+                        throw new JwtAuthenticationException(AuthErrorCode.ACCOUNT_LOGGED_IN_ANOTHER_DEVICE);
+                    }
                 }
 
-                if(!storedAccessToken.equals(accessToken)) {
-                    throw new BaseException(AuthErrorCode.ACCOUNT_LOGGED_IN_ANOTHER_DEVICE);
-                }
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userId, null,
+                                List.of(new SimpleGrantedAuthority("ROLE_" + role.toString())));
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
 
-            UsernamePasswordAuthenticationToken auth =
-                    new UsernamePasswordAuthenticationToken(userId, null,
-                            List.of(new SimpleGrantedAuthority("ROLE_" + role.toString())));
+            filterChain.doFilter(request, response);
 
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        } catch (JwtAuthenticationException ex) {
+            authenticationEntryPoint.commence(request, response, ex);
         }
-
-        filterChain.doFilter(request, response);
     }
 }
+
