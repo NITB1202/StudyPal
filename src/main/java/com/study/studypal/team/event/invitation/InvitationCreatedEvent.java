d094495a60cb9acd077385304073bff3b02c4515
nguyetmtran <nguyetmtran@kms-technology.com>
2025-09-22 00:20:01 +0700

    feat: add create invitation event

---
+++ src/main/java/com/study/studypal/team/event/invitation/InvitationCreatedEvent.java
@@ -0,0 +1,13 @@
+package com.study.studypal.team.event.invitation;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InvitationCreatedEvent {
+  private UUID invitationId;
+  private UUID inviterId;
+  private UUID teamId;
+}
--- src/main/java/com/study/studypal/team/service/api/impl/InvitationServiceImpl.java
+++ src/main/java/com/study/studypal/team/service/api/impl/InvitationServiceImpl.java
@@ -10,6 +10,7 @@ import com.study.studypal.team.dto.invitation.response.ListInvitationResponseDto
 import com.study.studypal.team.entity.Invitation;
 import com.study.studypal.team.entity.Team;
 import com.study.studypal.team.enums.TeamRole;
+import com.study.studypal.team.event.invitation.InvitationCreatedEvent;
 import com.study.studypal.team.exception.InvitationErrorCode;
 import com.study.studypal.team.repository.InvitationRepository;
 import com.study.studypal.team.service.api.InvitationService;
@@ -27,6 +28,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -40,6 +42,7 @@ public class InvitationServiceImpl implements InvitationService {
   private final TeamInternalService teamService;
   private final TeamNotificationSettingInternalService teamNotificationSettingService;
   private final ModelMapper modelMapper;
+  private final ApplicationEventPublisher eventPublisher;
 
   @PersistenceContext private final EntityManager entityManager;
 
@@ -72,6 +75,15 @@ public class InvitationServiceImpl implements InvitationService {
     } catch (DataIntegrityViolationException e) {
       throw new BaseException(InvitationErrorCode.INVITEE_ALREADY_INVITED);
     }
+
+    InvitationCreatedEvent event =
+        InvitationCreatedEvent.builder()
+            .invitationId(invitation.getId())
+            .inviterId(userId)
+            .teamId(request.getTeamId())
+            .build();
+
+    eventPublisher.publishEvent(event);
 
     return modelMapper.map(invitation, InvitationResponseDto.class);
   }
