3fb4e4be1384ea6c141dbd8c2faeb6c6b63d3b7b
nguyetmtran <nguyetmtran@kms-technology.com>
2025-09-22 18:02:27 +0700

    fix: change config path

--- src/main/java/com/study/studypal/notification/config/FirebaseInitializer.java
+++ src/main/java/com/study/studypal/notification/config/FirebaseInitializer.java
@@ -7,30 +7,36 @@ import com.study.studypal.common.exception.BaseException;
 import com.study.studypal.notification.exception.DeviceTokenErrorCode;
 import jakarta.annotation.PostConstruct;
 import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
+@RequiredArgsConstructor
 public class FirebaseInitializer {
   @Value("${firebase.config.path}")
   private String firebaseConfigPath;
 
+  private final ResourceLoader resourceLoader;
+
   @PostConstruct
   public void initialize() {
     try {
-      InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
+      Resource resource = resourceLoader.getResource(firebaseConfigPath);
+      try (InputStream serviceAccount = resource.getInputStream()) {
+        FirebaseOptions options =
+            FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
-      FirebaseOptions options =
-          FirebaseOptions.builder()
-              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-              .build();
-
-      if (FirebaseApp.getApps().isEmpty()) {
-        FirebaseApp.initializeApp(options);
+        if (FirebaseApp.getApps().isEmpty()) {
+          FirebaseApp.initializeApp(options);
+        }
       }
     } catch (Exception e) {
-      throw new BaseException(DeviceTokenErrorCode.PUSH_SERVICE_INIT_ERROR, e.getMessage());
+      throw new BaseException(DeviceTokenErrorCode.PUSH_SERVICE_INIT_ERROR, e.getMessage(), e);
     }
   }
 }
